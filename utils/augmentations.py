7ce90f1e50d8c2cd474af7c1170d9e6c62cdccc6
jeyakumar_johnny_jonathan <104885230+jjjonathan14@users.noreply.github.com>
2023-07-11 08:49:40 +0530

    commented perline noise

--- utils/augmentations.py
+++ utils/augmentations.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 from random import randint, uniform
-from perlin_noise import PerlinNoise
+# from perlin_noise import PerlinNoise
 
 
 def scale_to_range(_array, _min, _max):
@@ -77,91 +77,91 @@ def show_img(_img_array):
     cv.imshow('Image Display', _img_array)
     cv.waitKey(0)
 
-
-def add_dust(img_raw):
-    PERLIN_NOISE_RED_FACTOR = 10  # Integer value representing fraction of image size to use for noise size. Larger number gives coarser noise but faster performance
-    CROP_SIZE = 100  # Half of window height/width in centre of image where crop colour is taken from
-    BLUR_SIZE_MAX = 41  # Max blur pixels
-
-    # Opacity ranges (actual opacity is a random value between these limits)
-    NORMAL_OPACITY_MIN = 0.5
-    NORMAL_OPACITY_MAX = 0.8
-    SCREEN_OPACITY_MIN = 0.1
-    SCREEN_OPACITY_MAX = 0.3
-
-    # Percentage range to desaturate colour (actual percentage is a random value between these limits)
-    DESATURATE_FRAC_MIN = 0.1
-    DESATURATE_FRAC_MAX = 0.5
-
-    # Octave range for  perlin noise. Higher octave gives higher frequency noise variations
-    OCTAVES_MIN = 1
-    OCTAVES_MAX = 4
-
-    # Load raw image
-    img_raw = np.array(img_raw, dtype=np.float32)
-    img_raw /= 255
-    height, width, _ = np.shape(img_raw)
-    height_noise = int(height / PERLIN_NOISE_RED_FACTOR)
-    width_noise = int((height_noise + width / PERLIN_NOISE_RED_FACTOR / 2) / 2)
-
-    # Cut out the centre of the image so we can find the average colour here
-    height_centre = int(height / 2)
-    width_centre = int(width / 2)
-    img_centre = img_raw[height_centre - CROP_SIZE:height_centre + CROP_SIZE,
-                 width_centre - CROP_SIZE:width_centre + CROP_SIZE]
-
-    # Find colour for dust by taking average colour in centre of image
-    colour_centre_avg = np.average(img_centre, 0)
-    colour_centre_avg = np.average(colour_centre_avg, 0)
-    colour_centre_avg /= np.max(colour_centre_avg)
-
-    # Create perlin noise
-    rand_seed = randint(0, 10000)
-    rand_octaves = randint(OCTAVES_MIN, OCTAVES_MAX)
-    noise = PerlinNoise(octaves=rand_octaves, seed=rand_seed)
-    noise_array_sm = np.array(
-        [[noise([i / width_noise, j / height_noise]) for j in range(width_noise)] for i in range(height_noise)])
-    noise_array = cv.resize(noise_array_sm, (width, height))
-
-    # Offset and scale intensity of noise so values are between 0 and noise_intensity_max
-    noise_array = scale_to_range(noise_array, 0, 1)
-
-    # Create 3D white overlay to use as a mask for filtering which areas are dusty and which aren't
-    dust_mask_white = flat_array_to_3d(noise_array, [1, 1, 1])
-
-    # Turn greyscale noise into coloured overlay
-    dust_mask_coloured = flat_array_to_3d(noise_array, colour_centre_avg)
-
-    ############
-    # OVERLAYS #
-    ############
-    # Use Normal overlay method to add non-translucent dust
-    normal_opacity = uniform(NORMAL_OPACITY_MIN, NORMAL_OPACITY_MAX)
-    alpha_overlay = scale_to_range(dust_mask_white, 0, normal_opacity)
-
-    desaturate_frac_norm = uniform(DESATURATE_FRAC_MIN, DESATURATE_FRAC_MAX)
-    img_dusty_norm = blend_normal(img_raw, desaturate(dust_mask_coloured, desaturate_frac_norm), alpha_overlay)
-
-    # Use 'screen' overlay method to create dusty image
-    desaturate_frac_screen = uniform(DESATURATE_FRAC_MIN, DESATURATE_FRAC_MAX)
-    screen_opacity = uniform(SCREEN_OPACITY_MIN, SCREEN_OPACITY_MAX)
-    img_dusty_screen = blend_screen(img_dusty_norm, desaturate(dust_mask_coloured, desaturate_frac_screen),
-                                    screen_opacity)
-
-    ############
-    # BLURRING #
-    ############
-    # Create random blur size
-    blur_size = randint(min(BLUR_SIZE_MAX, 3), BLUR_SIZE_MAX)
-    if blur_size % 2 == 0:
-        blur_size += 1
-
-    # Blur image, and combine with original image using dust_mask_white as a mask
-    blur_kernel = (blur_size, blur_size)
-    img_dusty_blurred_all = cv.GaussianBlur(img_dusty_screen, blur_kernel, 0)
-    img_dusty_blurred = (dust_mask_white * img_dusty_blurred_all) + ((1 - dust_mask_white) * img_dusty_screen)
-
-    return img_dusty_blurred * 255
+#
+# def add_dust(img_raw):
+#     PERLIN_NOISE_RED_FACTOR = 10  # Integer value representing fraction of image size to use for noise size. Larger number gives coarser noise but faster performance
+#     CROP_SIZE = 100  # Half of window height/width in centre of image where crop colour is taken from
+#     BLUR_SIZE_MAX = 41  # Max blur pixels
+#
+#     # Opacity ranges (actual opacity is a random value between these limits)
+#     NORMAL_OPACITY_MIN = 0.5
+#     NORMAL_OPACITY_MAX = 0.8
+#     SCREEN_OPACITY_MIN = 0.1
+#     SCREEN_OPACITY_MAX = 0.3
+#
+#     # Percentage range to desaturate colour (actual percentage is a random value between these limits)
+#     DESATURATE_FRAC_MIN = 0.1
+#     DESATURATE_FRAC_MAX = 0.5
+#
+#     # Octave range for  perlin noise. Higher octave gives higher frequency noise variations
+#     OCTAVES_MIN = 1
+#     OCTAVES_MAX = 4
+#
+#     # Load raw image
+#     img_raw = np.array(img_raw, dtype=np.float32)
+#     img_raw /= 255
+#     height, width, _ = np.shape(img_raw)
+#     height_noise = int(height / PERLIN_NOISE_RED_FACTOR)
+#     width_noise = int((height_noise + width / PERLIN_NOISE_RED_FACTOR / 2) / 2)
+#
+#     # Cut out the centre of the image so we can find the average colour here
+#     height_centre = int(height / 2)
+#     width_centre = int(width / 2)
+#     img_centre = img_raw[height_centre - CROP_SIZE:height_centre + CROP_SIZE,
+#                  width_centre - CROP_SIZE:width_centre + CROP_SIZE]
+#
+#     # Find colour for dust by taking average colour in centre of image
+#     colour_centre_avg = np.average(img_centre, 0)
+#     colour_centre_avg = np.average(colour_centre_avg, 0)
+#     colour_centre_avg /= np.max(colour_centre_avg)
+#
+#     # Create perlin noise
+#     rand_seed = randint(0, 10000)
+#     rand_octaves = randint(OCTAVES_MIN, OCTAVES_MAX)
+#     noise = PerlinNoise(octaves=rand_octaves, seed=rand_seed)
+#     noise_array_sm = np.array(
+#         [[noise([i / width_noise, j / height_noise]) for j in range(width_noise)] for i in range(height_noise)])
+#     noise_array = cv.resize(noise_array_sm, (width, height))
+#
+#     # Offset and scale intensity of noise so values are between 0 and noise_intensity_max
+#     noise_array = scale_to_range(noise_array, 0, 1)
+#
+#     # Create 3D white overlay to use as a mask for filtering which areas are dusty and which aren't
+#     dust_mask_white = flat_array_to_3d(noise_array, [1, 1, 1])
+#
+#     # Turn greyscale noise into coloured overlay
+#     dust_mask_coloured = flat_array_to_3d(noise_array, colour_centre_avg)
+#
+#     ############
+#     # OVERLAYS #
+#     ############
+#     # Use Normal overlay method to add non-translucent dust
+#     normal_opacity = uniform(NORMAL_OPACITY_MIN, NORMAL_OPACITY_MAX)
+#     alpha_overlay = scale_to_range(dust_mask_white, 0, normal_opacity)
+#
+#     desaturate_frac_norm = uniform(DESATURATE_FRAC_MIN, DESATURATE_FRAC_MAX)
+#     img_dusty_norm = blend_normal(img_raw, desaturate(dust_mask_coloured, desaturate_frac_norm), alpha_overlay)
+#
+#     # Use 'screen' overlay method to create dusty image
+#     desaturate_frac_screen = uniform(DESATURATE_FRAC_MIN, DESATURATE_FRAC_MAX)
+#     screen_opacity = uniform(SCREEN_OPACITY_MIN, SCREEN_OPACITY_MAX)
+#     img_dusty_screen = blend_screen(img_dusty_norm, desaturate(dust_mask_coloured, desaturate_frac_screen),
+#                                     screen_opacity)
+#
+#     ############
+#     # BLURRING #
+#     ############
+#     # Create random blur size
+#     blur_size = randint(min(BLUR_SIZE_MAX, 3), BLUR_SIZE_MAX)
+#     if blur_size % 2 == 0:
+#         blur_size += 1
+#
+#     # Blur image, and combine with original image using dust_mask_white as a mask
+#     blur_kernel = (blur_size, blur_size)
+#     img_dusty_blurred_all = cv.GaussianBlur(img_dusty_screen, blur_kernel, 0)
+#     img_dusty_blurred = (dust_mask_white * img_dusty_blurred_all) + ((1 - dust_mask_white) * img_dusty_screen)
+#
+#     return img_dusty_blurred * 255
 
 
 if __name__ == '__main__':
@@ -169,5 +169,5 @@ if __name__ == '__main__':
     path_img_input = "./01_InputImages/00_sample.jpg"
     path_img_output = "./02_OutputImages/01_sample.jpg"
     input_file = cv.imread(path_img_input)
-    output_file = add_dust(input_file)
+    # output_file = add_dust(input_file)
     cv.imwrite(path_img_output, output_file)
